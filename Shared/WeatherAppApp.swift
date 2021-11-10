96e90ffb4d6904b59f53c7d1d7cb06f1f311b25f
satvikkathpal <satvikkathpal0109@gmail.com>
2021-11-10 15:33:56 -0500

    Initial Commit

---
+++ Shared/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  Shared
+//
+//  Created by Satvik Kathpal on 2021-11-10.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
